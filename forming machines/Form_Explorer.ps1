f55657df0e7c916c91c5a513530472531c90e3ce
ShortArrow <bamboogeneral@live.jp>
2022-04-26 13:44:01 +0900

    add onedrive form

--- forming machines/Form_Explorer.ps1
+++ forming machines/Form_Explorer.ps1
@@ -1,38 +1,62 @@
 function Get-ShowFrequentState {
     param ()
     $regpath = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\"
-    $state=(Get-Item -Path $regpath).GetValue("ShowFrequent")
+    $state = (Get-Item -Path $regpath).GetValue("ShowFrequent")
     return [ShowFrequentState]$state
 }
 
 enum ShowFrequentState {
     # Default is 1
-    isHidden=0
-    Shown=1
+    isHidden = 0
+    isShown = 1
 }
 
 function Get-ShowRecentState {
     param ()
     $regpath = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\"
-    $state=(Get-Item -Path $regpath).GetValue("ShowRecent")
+    $state = (Get-Item -Path $regpath).GetValue("ShowRecent")
     return [ShowRecentState]$state
 }
 
 enum ShowRecentState {
     # Default is 1
-    isHidden=0
-    Shown=1
+    isHidden = 0
+    isShown = 1
 }
 
 function Get-HideFileExtState {
     param ()
     $regpath = "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\"
-    $state=(Get-Item -Path $regpath).GetValue("HideFileExt")
+    $state = (Get-Item -Path $regpath).GetValue("HideFileExt")
     return [HideFileExtState]$state
 }
 
 enum HideFileExtState {
     # Default is 1
-    Shown=0
-    isHidden=1
+    isShown = 0
+    isHidden = 1
+}
+
+function Get-OneDriveState {
+    param ()
+    $regpath = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OneDrive\"
+    $state = (Get-Item -Path $regpath).GetValue("DisableFileSyncNGSC")
+    return [OneDriveState]$state
+}
+
+enum OneDriveState {
+    # Default is 1
+    Disable = 0
+    Enable = 1
+}
+
+function Get-OneDriveLinkState {
+    param ()
+    $state = (Test-Path "$env:USERPROFILE\Links\OneDrive.lnk")
+    return ($state ? [OneDriveLinkState]::Found : [OneDriveLinkState]::NotFound)
+}
+enum OneDriveLinkState {
+    # Default is Found
+    Found
+    NotFound
 }
--- test/Form_Explorer.Tests.ps1
+++ test/Form_Explorer.Tests.ps1
@@ -1,6 +1,6 @@
 . bin/glass_factory.ps1
 
-Describe "frequents" {
+Describe "Explorer" {
     BeforeAll {
         [string]$machine = Get-FormingMachine($PSCommandPath)
         . $machine
@@ -9,10 +9,16 @@ Describe "frequents" {
     It "Hide frequent directorys or files" {
         [string](Get-ShowFrequentState) | Should -Be "isHidden"
     }
-    It "Hide recent acceess derectory of file are shown" {
+    It "Hide recent acceess derectory of file are isShown" {
         [string](Get-ShowRecentState) | Should -Be "isHidden"
     }
-    It "All extensions of file are shown" {
-        [string](Get-HideFileExtState) | Should -Be "Shown"
+    It "All extensions of file are isShown" {
+        [string](Get-HideFileExtState) | Should -Be "isShown"
+    }
+    It "Not Exists Onedrive Link" {
+        [string](Get-OneDriveLinkState) | Should -Be "NotFound"
+    }
+    It "Disable Onedrive" {
+        [string](Get-OneDriveState) | Should -Be "Enable"
     }
 }
